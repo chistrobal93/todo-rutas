624759922006706c813e1e7846ca88df21282676
Cristobal <c.garcia_93@hotmail.com>
2022-06-15 23:08:33 -0400

    Muestra mapa en un modal

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const engine = require('ejs-mate');
 const logger = require("./logger");
+const path = require("path");
 
 // Express App
 const app = express();
@@ -11,27 +12,18 @@ const PORT = process.env.PORT || 3000;
 app.set('appName', 'TodoRutas');
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
-app.set("views","./src/views");
-
-app.listen(PORT, () => {
-  logger.info(`Servidor iniciado en http://localhost:${PORT}`);
-});
+app.set("views", path.join(__dirname,'views'));
 
 // Middleware
-  // Directorios públicos que puede acceder el navegador
+// Directorios públicos que puede acceder el navegador
 app.use('/', express.static('public'));
 app.use('/', express.static('node_modules/@popperjs/core/dist/umd/'));
 app.use('/', express.static('node_modules/bootstrap/dist/js'));
-  // Para aceptar formularios metodo POST
+// Para aceptar formularios metodo POST
 app.use(express.urlencoded({ extended: true}));
-  // Body parser json
+// Body parser json
 app.use(express.json());
-
-// app.use((req, res, next) => {
-//   res.locals.path = req.path;
-//   next();
-// });
-
+  
 // Rutas
 app.use('/', require('./routes/indexRoutes'));
 app.use('/mantenedor', require('./routes/mantenedorRoutes'));
@@ -40,4 +32,8 @@ app.use('/mantenedor', require('./routes/mantenedorRoutes'));
 app.use((req, res) => {
   logger.error(`RUTA NO ENCONTRADA - ruta: ${req.path}`);
   res.status(404).render('404', { title: '404' });
+});
+
+app.listen(PORT, () => {
+  logger.info(`Servidor iniciado en http://localhost:${PORT}`);
 });
