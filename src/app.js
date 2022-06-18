6abac94f46b6d9b1105a460f9a12b3c302b4beff
cristobal <c.garcia_93@hotmail.com>
2022-06-18 16:34:39 -0400

    Test commit prototipo

--- src/app.js
+++ src/app.js
@@ -1,39 +1,39 @@
-const express = require('express');
-require('dotenv').config();
-const engine = require('ejs-mate');
-const logger = require("./logger");
-const path = require("path");
+// const express = require('express');
+// require('dotenv').config();
+// const engine = require('ejs-mate');
+// const logger = require("./logger");
+// const path = require("path");
 
-// Express App
-const app = express();
+// // Express App
+// const app = express();
 
-// Settings
-const PORT = process.env.PORT || 3000;
-app.set('appName', 'TodoRutas');
-app.engine('ejs', engine);
-app.set('view engine', 'ejs');
-app.set("views", path.join(__dirname,'views'));
+// // Settings
+// const PORT = process.env.PORT || 3000;
+// app.set('appName', 'TodoRutas');
+// app.engine('ejs', engine);
+// app.set('view engine', 'ejs');
+// app.set("views", path.join(__dirname,'views'));
 
-// Middleware
-// Directorios públicos que puede acceder el navegador
-app.use('/', express.static('public'));
-app.use('/', express.static('node_modules/@popperjs/core/dist/umd/'));
-app.use('/', express.static('node_modules/bootstrap/dist/js'));
-// Para aceptar formularios metodo POST
-app.use(express.urlencoded({ extended: true}));
-// Body parser json
-app.use(express.json());
+// // Middleware
+// // Directorios públicos que puede acceder el navegador
+// app.use('/', express.static('public'));
+// app.use('/', express.static('node_modules/@popperjs/core/dist/umd/'));
+// app.use('/', express.static('node_modules/bootstrap/dist/js'));
+// // Para aceptar formularios metodo POST
+// app.use(express.urlencoded({ extended: true}));
+// // Body parser json
+// app.use(express.json());
   
-// Rutas
-app.use('/', require('./routes/indexRoutes'));
-app.use('/mantenedor', require('./routes/mantenedorRoutes'));
+// // Rutas
+// app.use('/', require('./routes/indexRoutes'));
+// app.use('/mantenedor', require('./routes/mantenedorRoutes'));
 
-// 404 page
-app.use((req, res) => {
-  logger.error(`RUTA NO ENCONTRADA - ruta: ${req.path}`);
-  res.status(404).render('404', { title: '404' });
-});
+// // 404 page
+// app.use((req, res) => {
+//   logger.error(`RUTA NO ENCONTRADA - ruta: ${req.path}`);
+//   res.status(404).render('404', { title: '404' });
+// });
 
-app.listen(PORT, () => {
-  logger.info(`Servidor iniciado en http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   logger.info(`Servidor iniciado en http://localhost:${PORT}`);
+// });
